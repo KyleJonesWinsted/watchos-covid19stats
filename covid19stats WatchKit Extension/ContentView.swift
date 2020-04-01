6fe129c7f8439fd5185374a055bb1086391bf7c4
Kyle Jones <KyleJonesWinsted@gmail.com>
2020-04-01 16:48:24 -0500

    Initial Commit

---
+++ covid19stats WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  covid19stats WatchKit Extension
+//
+//  Created by Kyle Jones on 4/1/20.
+//  Copyright © 2020 Kyle Jones. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ covid19stats WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  covid19stats WatchKit Extension
+//
+//  Created by Kyle Jones on 4/1/20.
+//  Copyright © 2020 Kyle Jones. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
