4ff7a97685ac6194b9c5bfa9acd86d87a7efa337
Kyle Jones <KyleJonesWinsted@gmail.com>
2020-04-01 19:39:53 -0500

    initial commit

--- covid19stats WatchKit Extension/ContentView.swift
+++ covid19stats WatchKit Extension/ContentView.swift
@@ -9,13 +9,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var testModel = TestModel()
+    @State var isDetailViewPresented = false
+    
     var body: some View {
-        Text("Hello, World!")
+        List {
+            ForEach(testModel.testCellNames, id: \.self) { cellName in
+                NavigationLink(destination: TestDetailView(text: cellName)) {
+                    Text(cellName)
+                }
+            }
+            .onMove(perform: { self.testModel.testCellNames.move(fromOffsets: $0, toOffset: $1)})
+            .onDelete(perform: { self.testModel.testCellNames.remove(atOffsets: $0)})
+            
+            Button(action: {
+                self.isDetailViewPresented = true
+            }) {
+                Text("Show sheet")
+            }
+            .sheet(isPresented: self.$isDetailViewPresented) {
+                TestSheet(text: "Hello", isDetailViewPresented: self.$isDetailViewPresented)
+            }
+        }
+    }
+}
+
+struct TestDetailView: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+        .navigationBarTitle(text)
+    }
+}
+
+struct TestSheet: View {
+    var text: String
+    @Binding var isDetailViewPresented: Bool
+    
+    var body: some View {
+        Button(action: {
+            self.isDetailViewPresented = false
+        }) {
+            Text(text)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            TestDetailView(text: "Hello")
+            TestSheet(text: "Hi Again", isDetailViewPresented: .constant(false))
+        }
     }
 }
--- covid19stats WatchKit Extension/HostingController.swift
+++ covid19stats WatchKit Extension/HostingController.swift
@@ -12,6 +12,7 @@ import SwiftUI
 
 class HostingController: WKHostingController<ContentView> {
     override var body: ContentView {
-        return ContentView()
+        let testModel = TestModel()
+        return ContentView(testModel: testModel)
     }
 }
