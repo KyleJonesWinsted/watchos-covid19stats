c327b44c461e962b80b92a150b70bfe8cc05670a
Kyle Jones <KyleJonesWinsted@gmail.com>
2020-04-04 19:50:45 -0500

    Added default networking

--- covid19stats WatchKit Extension/CountriesController.swift
+++ covid19stats WatchKit Extension/CountriesController.swift
@@ -7,20 +7,69 @@
 //
 
 import Foundation
+import Combine
 
 final class CountriesController: ObservableObject {
+    
+    let objectWillChange = ObservableObjectPublisher()
     
     @Published var countries: [Country]
     @Published var namesOfCountries: [String]
     
     init() {
-        self.countries = [Country(name: "All")]
+        self.countries = []
         self.namesOfCountries = []
         self.getCountryNames()
+        self.updateAllStats()
+        let nc = NotificationCenter.default
+        nc.addObserver(self, selector: #selector(countryUpdated), name: Notification.Name("Country Updated"), object: nil)
+    }
+    
+    @objc private func countryUpdated() {
+        DispatchQueue.main.async {
+            self.objectWillChange.send()
+        }
     }
     
     private func getCountryNames() {
-        self.namesOfCountries += ["China", "South Korea", "USA", "Italy"]
+        self.getCountriesJSON { (result) in
+            switch result {
+            case .success(let json):
+                let response = json["response"] as! Array<String>
+                self.namesOfCountries = response
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    private func getCountriesJSON(completionHandler: @escaping (Result<[String: Any], Error>) -> Void) {
+        //self.namesOfCountries += ["China", "South Korea", "USA", "Italy"]
+        let url = URL(string: "https://covid-193.p.rapidapi.com/countries")!
+        let headers = [
+            "x-rapidapi-host": "covid-193.p.rapidapi.com",
+            "x-rapidapi-key": ConfigString.rapidApiKey.rawValue
+        ]
+        var request = URLRequest(url: url)
+        request.allHTTPHeaderFields = headers
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completionHandler(.failure(error))
+            }
+            guard let httpResponse = response as? HTTPURLResponse,
+                (200...299).contains(httpResponse.statusCode) else {
+                    completionHandler(.failure(NetworkError.badResponseCode(response)))
+                    return
+            }
+            if let data = data {
+                if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                    completionHandler(.success(json))
+                }
+            }
+               
+        }
+        task.resume()
+        
     }
     
     public func updateAllStats() {
@@ -35,8 +84,8 @@ final class CountriesController: ObservableObject {
     
     public func addNewCountry(name: String) {
         var newCountry = Country(name: name)
-        newCountry.updateStats()
         self.countries.append(newCountry)
+        newCountry.updateStats()
     }
     
 }
--- covid19stats WatchKit Extension/Country.swift
+++ covid19stats WatchKit Extension/Country.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Country {
+class Country {
     
     var id = UUID()
     var name: String
@@ -22,12 +22,72 @@ struct Country {
         return formatter.string(from: updateTime)
     }
     
-    public mutating func updateStats() {
-        let newCases = Cases(new: "+\(Int.random(in: 1...5000))", active: Int.random(in: 400...1000000), critical: Int.random(in: 1...3000), recovered: Int.random(in: 1...5000), total: Int.random(in: 500...1000000))
-        let newDeaths = Deaths(new: "+\(Int.random(in: 1...500))", total: Int.random(in: 1...3000))
-        self.cases = newCases
-        self.deaths = newDeaths
+    init(name: String) {
+        self.name = name
+    }
+    
+    private func setStats(cases: Cases, deaths: Deaths) {
+        self.cases = cases
+        self.deaths = deaths
         self.updateTime = Date()
+    }
+    
+    private func sendCountryUpdatedNotification() {
+        let nc = NotificationCenter.default
+        nc.post(name: Notification.Name("Country Updated"), object: nil)
+    }
+    
+    public func updateStats() {
+        self.getStatsJson { (result) in
+            switch result {
+                case .success(let json):
+                    let responseArray = json["response"] as? Array<Any>
+                    let response = responseArray![0] as! [String: Any]
+                    let casesJson = response["cases"] as! [String: Any]
+                    let deathsJson = response["deaths"] as! [String: Any]
+                    let cases = Cases(new: casesJson["new"] as! String,
+                                      active: casesJson["active"] as! Int,
+                                      critical: casesJson["critical"] as! Int,
+                                      recovered: casesJson["recovered"] as! Int,
+                                      total: casesJson["total"] as! Int)
+                    let deaths = Deaths(new: deathsJson["new"] as! String,
+                                        total: deathsJson["total"] as! Int)
+                    self.setStats(cases: cases, deaths: deaths)
+                    self.sendCountryUpdatedNotification()
+                case .failure(let error):
+                    print(error)
+            }
+        }
+    }
+    
+    private func getStatsJson(completionHandler: @escaping (Result<[String: Any], Error>) -> Void) {
+        var components = URLComponents(string: "https://covid-193.p.rapidapi.com/statistics")!
+        components.queryItems = [
+            URLQueryItem(name: "country", value: self.name)
+        ]
+        let url = components.url!
+        var request = URLRequest(url: url)
+        let headers: [String: String] = [
+            "x-rapidapi-host": "covid-193.p.rapidapi.com",
+            "x-rapidapi-key": ConfigString.rapidApiKey.rawValue
+        ]
+        request.allHTTPHeaderFields = headers
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completionHandler(.failure(error))
+            }
+            guard let httpResponse = response as? HTTPURLResponse,
+                (200...299).contains(httpResponse.statusCode) else {
+                    completionHandler(.failure(NetworkError.badResponseCode(response)))
+                    return
+            }
+            if let data = data {
+                if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
+                    completionHandler(.success(json))
+                }
+            }
+        }
+        task.resume()
     }
 }
 
@@ -40,3 +100,8 @@ struct Deaths {
     var new: String
     var total: Int
 }
+
+enum NetworkError: Error {
+    case badResponseCode(URLResponse?)
+}
+
